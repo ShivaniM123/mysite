71408ad18133ed5a6c116539f8a7fca48270ef75
ShivaniM123 <shivanimaheshwari404@gmail.com>
2025-06-17 11:17:10 +0530

    fix: cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,45 +1,51 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
+
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
+
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+      }
     });
 
-    // create a div
-    const newDiv = document.createElement('div');
-    newDiv.className = 'cards-card-para hidden';
     const bodyDiv = li.querySelector('.cards-card-body');
 
-    const len = bodyDiv.children.length;
-    for (let i = 0; i < len - 2; i += 1) {
-      if (bodyDiv.children.length > 0) {
-      if (bodyDiv.children.length > 0) {
-        const para = bodyDiv.children[2];
-        newDiv.append(para);
-      }
+    // Create hidden div
+    const newDiv = document.createElement('div');
+    newDiv.className = 'cards-card-para hidden';
+
+    // Move all paragraphs after the first two into the hidden div
+    while (bodyDiv.children.length > 2) {
+      newDiv.append(bodyDiv.children[2]); // index 2 because index 0 and 1 are preserved
     }
+
     bodyDiv.append(newDiv);
-    const subbodyDiv = li.querySelector('.cards-card-para');
 
     // Read More button
     const button = document.createElement('button');
     button.className = 'report-button';
-    bodyDiv.append(button);
     button.textContent = '> Read More';
+
     button.addEventListener('click', () => {
-      subbodyDiv.classList.toggle('hidden');
-      button.textContent = subbodyDiv.classList.contains('hidden') ? 'Read More >' : 'Read Less <';
+      newDiv.classList.toggle('hidden');
+      button.textContent = newDiv.classList.contains('hidden') ? 'Read More >' : 'Read Less <';
     });
+
+    bodyDiv.append(button);
 
     ul.append(li);
   });
+
+  // Optimize pictures
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+
   block.textContent = '';
   block.append(ul);
 }
